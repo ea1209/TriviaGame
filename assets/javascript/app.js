4974ff2c5ced35558aa896413a5ce23ca0430f0d
Emil Ashford <emil.ashford@avalara.com>
2017-09-09 09:59:18 -0400

    homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
 $("#start").on("click",start);
 $("#submit").on("click",submit);
 
-var time = 11;
+var time = 31;
 var right = 0;
 var wrong = 0; 
 
+
+
 function start () {
-time = 11;
+time = 31;
 counter = setInterval(timer,1000);
 
 }
@@ -28,24 +30,63 @@ $("#time").html("Time Remaining: " + time);
 
 function submit () {
 
-	$("#right").html("Right: " + right);
-	$("#wrong").html("Wrong: " + wrong);
 
 	console.log($('input[name="q1"]:checked')[0].nextSibling.innerText);
 	console.log($('input[name="q2"]:checked')[0].nextSibling.innerText);
-	console.log($('input[name="q3"]:checked')[0].nextSibling.innerText);
-	console.log($('input[name="q4"]:checked')[0].nextSibling.innerText);
-	console.log($('input[name="q5"]:checked')[0].nextSibling.innerText);
-	
-	if ($('input[name="q1"]:checked')[0].nextSibling.innerText === "Derick Rose") {
+
+
+	if ($('input[name="q1"]:checked')[0].nextSibling.innerText === "Kobe Bryant") {
 
 		right ++; 
 
 	}
 
-	else {
-		wrong --; 
+			else {
+			wrong ++; 
+			}
+
+	if ($('input[name="q2"]:checked')[0].nextSibling.innerText === "Derick Rose") {
+
+		right ++; 
+
 	}
+
+			else {
+			wrong ++; 
+			}
+
+	if ($('input[name="q3"]:checked')[0].nextSibling.innerText === "John Salley") {
+
+		right ++; 
+
+	}
+
+			else {
+			wrong ++; 
+			}
+
+	if ($('input[name="q4"]:checked')[0].nextSibling.innerText === "Amir Johnson") {
+
+		right ++; 
+
+	}
+
+			else {
+			wrong ++; 
+			}
+
+	if ($('input[name="q5"]:checked')[0].nextSibling.innerText === "Doc Rivers") {
+
+		right ++; 
+
+	}
+
+			else {
+			wrong ++; 
+			}
+
+	$("#right").html("Right: " + right);
+	$("#wrong").html("Wrong: " + wrong);
 }
 
 
